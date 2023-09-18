d60b4af786beff6e3bfcd546dd00a1fe63bc3edd
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2023-09-18 15:58:23 -0600

    Update SwiftSystemSettingsPlugin.swift
    Connected "app-notifications" to open notifications settings directly on iOS

--- ios/Classes/SwiftSystemSettingsPlugin.swift
+++ ios/Classes/SwiftSystemSettingsPlugin.swift
@@ -9,16 +9,31 @@ public class SwiftSystemSettingsPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      openSettings()
+      openSettings(call.method)
   }
 
-  private func openSettings() {
-    if let url = URL(string: UIApplication.openSettingsURLString) {
-        if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        } else {
-            UIApplication.shared.openURL(url)
+  private func openSettings(_ method: String) {
+    var url = URL(string: UIApplication.openSettingsURLString)
+
+    switch method {
+      case "app-notifications":
+        if #available(iOS 16, *) {
+          url = URL(string: UIApplication.openNotificationSettingsURLString)
         }
+        if #available(iOS 15.4, *) {
+          url = URL(string: UIApplicationOpenNotificationSettingsURLString)
+        }
+      default:
+        break
     }
+
+    if let url = url {
+      if #available(iOS 10.0, *) {
+          UIApplication.shared.open(url, options: [:], completionHandler: nil)
+      } else {
+          UIApplication.shared.openURL(url)
+      }
+    }
+    
   }
 }
